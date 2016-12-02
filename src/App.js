3597bbbea2205d925a74ac83e8d58761ba0a11e2
juan267 <j.gomez267@gmail.com>
2016-12-02 11:35:41 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,17 +3,90 @@ import React, { Component } from 'react';
 /// Modifica el componente para que se puedan agregar tareas
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      newTask: '',
+      tasks: [
+        { id: 1, name: "Sacar la ropa", done: false },
+        { id: 2, name: "Hacer la cama", done: true },
+        { id: 3, name: "Leer un rato", done: false }
+      ],
+      errors: {
+        newTask: false
+      }
+    }
+  }
+  validateNewTask() {
+    if (this.state.newTask === '') {
+      this.setState({
+        errors: {
+          newTask: true
+        }
+      })
+      return false
+    }
+    return true
+  }
+  addTask(event) {
+    event.preventDefault()
+    if (this.validateNewTask()) {
+      let oldTasks = this.state.tasks
+      let newTask = {
+        id: Math.max(...oldTasks.map(task => task.id)) + 1,
+        name: this.state.newTask,
+        done: false
+      }
+      this.setState({
+        tasks: [...oldTasks, newTask],
+        newTask: ''
+      })
+    }
+  }
+  updateTask(event) {
+    this.setState({
+      newTask: event.target.value,
+      errors: {
+        newTask: false
+      }
+    })
+  }
+  toogleDone(id,event) {
+    const newTasks = this.state.tasks.map(task => {
+      if (task.id === id) {
+        task.done = !task.done
+        return task
+      }
+      return task
+    })
+    this.setState({
+      tasks: newTasks
+    })
+  }
+  renderTasks() {
+    const tasks = this.state.tasks.map((task, index) => {
+      return (
+        <li
+          className={task.done ? 'done' : null}
+          key={task.id}
+          onClick={this.toogleDone.bind(this, task.id)}>
+          {task.name}
+        </li>
+      )
+    })
+    return tasks
+  }
   render() {
     return (
-      <div class="wrapper">
-        <div class="list">
+      <div className="wrapper">
+        <div className="list">
           <h3>Por hacer:</h3>
-          <ul class="todo">
-            <li>Sacar la ropa</li>
-            <li>Hacer la cama</li>
-            <li>Leer un rato</li>
+          <ul className="todo">
+            {this.renderTasks()}
           </ul>
-          <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" />
+          <form onSubmit={this.addTask.bind(this)}>
+          <input className={this.state.errors.newTask ? 'error' : null} type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" value={this.state.newTask} onChange={this.updateTask.bind(this)}/>
+          </form>
         </div>
       </div>
     )
--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { render } from 'enzyme';
-import { shallow } from 'enzyme';
+import { render, shallow, mount } from 'enzyme';
 import App from './App';
 
 it('renders without crashing', () => {
@@ -14,16 +13,58 @@ it('renders one ul tag', () => {
   expect(wrapper.find('ul').length).toBe(1);
 });
 
-it('renders four li tags', () => {
+it('renders three li tags at the beggining', () => {
   const wrapper = render(<App />);
-  expect(wrapper.find('li').length).toBe(4);
+  expect(wrapper.find('li').length).toBe(3);
 });
 
-it('rendered the li with correct text', () => {
+it('rendered the li with correct text at the beggining', () => {
   const wrapper = shallow(<App />);
-  const techs = ['Javascript', 'CSS', 'HTLM', 'React']
-  expect(wrapper.find('li').length).toBe(4);
+  const tasks = ['Sacar la ropa', 'Hacer la cama', 'Leer un rato',]
+  expect(wrapper.find('li').length).toBe(3);
   wrapper.find('li').forEach(function(node, i) {
-    expect(node.text()).toBe(techs[i]);
+    expect(node.text()).toBe(tasks[i]);
   });
 });
+
+it('creates a new task with the correct text', () => {
+  const wrapper = mount(<App />);
+  const tasks = ['Sacar la ropa', 'Hacer la cama', 'Leer un rato', 'Hola']
+  const newTask = wrapper.find('#new-task');
+  newTask.node.value = 'Hola';
+  newTask.simulate('change', {target: {value: 'Hola'}})
+  wrapper.find('form').simulate('submit', newTask)
+  expect(wrapper.find('li').length).toBe(4);
+  wrapper.find('li').forEach(function(node, i) {
+    expect(node.text()).toBe(tasks[i]);
+  });
+})
+
+it('the text input value is reset after creating task', () => {
+  const wrapper = mount(<App />);
+  const newTask = wrapper.find('#new-task');
+  newTask.node.value = 'Hola';
+  newTask.simulate('change', {target: {value: 'Hola'}})
+  wrapper.find('form').simulate('submit', newTask)
+  expect(wrapper.find('#new-task').node.value).toBe('')
+})
+
+it('adds error class to input field when creating a blank task', () => {
+  const wrapper = mount(<App />)
+  const newTask = wrapper.find('#new-task');
+  wrapper.find('form').simulate('submit')
+  expect(newTask.hasClass('error')).toBe(true)
+})
+
+
+it('toggles done classs on li when click', () => {
+  const wrapper = mount(<App />);
+  const task = wrapper.find('li').first()
+  task.simulate('click')
+  expect(task.hasClass('done')).toBe(true)
+  task.simulate('click')
+  expect(task.hasClass('done')).toBe(false)
+})
+
+
+
