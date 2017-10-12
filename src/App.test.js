f9140b433b3f9d875de371f6595d56cdd75c78de
jeferbc <jeffe.bernal@gmail.com>
2017-10-12 13:16:58 -0500

    update test

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { render, shallow, mount } from 'enzyme';
-import {Router, Route, hashHistory, IndexRedirect, createMemoryHistory} from 'react-router'
-import { Simulate } from 'react-addons-test-utils'
+import {Router, Route, hashHistory, IndexRedirect, createMemoryHistory} from 'react-router';
 import {App, Page1, Page2, NotFound} from './App';
 import Setup from './Setup';
-
-const { click } = Simulate
-
 
 const routes = (
   <Router history={hashHistory}>
@@ -31,16 +27,16 @@ it('renders Pagina 1 on load', () => {
 })
 
 it('changes to Page2 component when clicking on page2 button', () => {
-  const wrapper = mount(routes)
-  click(wrapper.find('a[href="#/page2"]').get(0), { button: 0 })
-  expect(wrapper.find('h1').text()).toBe('Pagina 2')
+  const wrapper = mount(routes);
+  wrapper.find('a[href="#/page2"]').simulate('click', { button: 0 })
+  expect(wrapper.find('h1').text()).toBe('Pagina 2');
 })
 
 it('changes to Page1 component when clicking on page1 button', () => {
   const wrapper = mount(routes)
-  click(wrapper.find('a[href="#/page2"]').get(0), { button: 0 })
+  wrapper.find('a[href="#/page2"]').simulate('click', { button: 0 })
   expect(wrapper.find('h1').text()).toBe('Pagina 2')
-  click(wrapper.find('a[href="#/page1"]').get(0), { button: 0 })
+  wrapper.find('a[href="#/page1"]').simulate('click', { button: 0 })
   expect(wrapper.find('h1').text()).toBe('Pagina 1')
 })
 
